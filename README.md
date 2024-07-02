17a3a55dc9e2d3e3990318db73d5db6bff652daf
valentingenev <valentin_genev@abv.bg>
2024-07-02 20:09:25 +0300

    [=] slight refactoring in the script; [+] readme now helps the usage;

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Chrome extension for Facebook
-Lets Facebook users hide posts from their feed based on string matches withing
+Lets Facebook users hide posts from their feed based on string matches within
 the posts.
 
 ## Possible settings
 **Selector:** h3 + div + div > .x1lliihq
-**Filters:** Sponsored, Join, Follow, Suggested for you, Reels and short videos, Sponsored, Follow, posted a video to the playlist, is playing, added a new photo to the album
+**Filters:** Sponsored, Join, Follow, Suggested for you, Reels and short videos, posted a video to the playlist, is playing, added a new photo to the album, is at, likes, sponsored_ad
+
+- 'Sponsored' and 'sponsored_ad' hide sponsored ads
+- 'Join', 'Follow' and 'Suggested for you' hide suggestions
+- 'posted a video to the playlist', 'is playing', 'added a new photo to the album' and 'is at' hides business accounts mascaraing as real people
+- 'likes' hides business accounts liked by real people
--- assets/js/read-feed.js
+++ assets/js/read-feed.js
@@ -1,38 +1,35 @@
-let settings
-
 chrome.storage.local.get(
     ['filters', 'selector'],
     s => {
-        settings = s
-        const firstPosts = document.querySelectorAll(settings.selector)
-        hidePosts(firstPosts)
-        filterNewPosts(firstPosts[0]?.parentNode)
+        const cleaner = new FeedCleaner(s)
+        cleaner.clean()
+        cleaner.observe()
     }
 )
 
-function filterNewPosts(postsWrapper) {
-    const observer = new MutationObserver((mutationList) => {
-        mutationList.forEach(mutation => hidePosts(mutation.addedNodes))
-    })
+class FeedCleaner {
+    constructor(settings) {
+        this.settings = settings
+        this.initialPosts = document.querySelectorAll(settings.selector)
+        this.filters = settings.filters.split(',').map(poster => poster.trim())
+    }
 
-    observer.observe(postsWrapper, { childList: true })
-}
-
-function hidePosts(posts) {
-    posts.forEach(post => {
-        const content = post.innerHTML
-        const filters = getFilters()
-
-        filters.forEach(filter => {
-            if (content.indexOf(filter) !== -1) {
-                post.style.display = 'none'
-                console.info('FB-FEED-FILTER:', 'post hidden')
-            }
+    observe() {
+        const observer = new MutationObserver((mutationList) => {
+            mutationList.forEach(mutation => this.clean(mutation.addedNodes))
         })
-    })
-}
 
-function getFilters() {
-    return settings.filters ?
-        settings.filters.split(',').map(poster => poster.trim()) : []
+        observer.observe(this.initialPosts[0].parentNode, { childList: true })
+    }
+
+    clean(posts = this.initialPosts) {
+        posts.forEach(post => {
+            this.filters.forEach(filter => {
+                if (post.innerHTML.indexOf(filter) !== -1) {
+                    post.style.display = 'none'
+                    console.info('FB-FEED-FILTER:', 'post hidden')
+                }
+            })
+        })
+    }
 }
