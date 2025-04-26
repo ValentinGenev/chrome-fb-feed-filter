8fab00f8032cce9bb086890414911b673c7dcd9b
valentingenev <valentin_genev@abv.bg>
2025-04-26 15:37:24 +0300

    [+] filter for the sponsored label;

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ the posts.
 
 ## Possible settings
 **Selector:** h3 + div + div > .x1lliihq <br>
-**Filters:** Sponsored, Join, Follow, Suggested for you, Reels and short videos, posted a video to the playlist, is playing, added a new photo to the album, is at, likes, sponsored_ad
+**Filters:** Join, Follow, Suggested for you, Reels and short videos, posted a video to the playlist, is playing, added a new photo to the album, is at, likes, Reels
 
-- 'Sponsored' and 'sponsored_ad' hide sponsored ads
 - 'Join', 'Follow' and 'Suggested for you' hide suggestions
 - 'posted a video to the playlist', 'is playing', 'added a new photo to the album' and 'is at' hides business accounts mascaraing as real people
 - 'likes' hides business accounts liked by real people
+
+## Sponsored
+The sponsored posts are hidden by their arial attribute.
--- assets/js/FeedCleaner.js
+++ assets/js/FeedCleaner.js
@@ -13,17 +13,59 @@ class FeedCleaner {
     }
 
     clean(posts = this.initialPosts) {
-        posts.forEach(post => {
-            this.filters.forEach(f => {
-                if (post.innerHTML.indexOf(f) !== -1) {
-                    this.isDebugMode ? post.style.width = '50%' : post.style.display = 'none'
-                    console.info('FB-FEED-FILTER:', 'post hidden')
+        for (const post of posts) {
+            for (const filter of this.filters) {
+                if (post.innerHTML.indexOf(filter) >= 0) {
+                    this.hidePost(post)
+                    break
                 }
-            })
-        })
+            }
+
+            this.checkForLabels(post)
+        }
+    }
+
+    async checkForLabels(post) {
+        if (await this.hasSponsoredLabel(post)) {
+            this.hidePost(post)
+        }
+    }
+
+    /**
+     * Finds the Sponsored label by label ID
+     * @param {Node} post
+     * @returns
+     */
+    async hasSponsoredLabel(post) {
+        const labelledElements = Array.from(post.querySelectorAll('span[aria-labelledby]')) ?? []
+        for (const element of labelledElements) {
+            const labelId = element.getAttribute('aria-labelledby')
+            let label = undefined
+            let tries = 0
+            while (!label && tries < 100) {
+                // Makes sure to wait for the label element creation.
+                // A mutator would be better if it was easier to pinpoint the label elements
+                await sleep(100)
+                label = document.querySelector("*[id='" + labelId + "']")?.innerText?.toLocaleLowerCase()
+                tries++
+                if (label === 'sponsored') {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    hidePost(post) {
+        this.isDebugMode ? post.style.width = '50%' : post.style.display = 'none'
+        console.info('FB-FEED-FILTER:', 'post hidden')
     }
 
     stopObserving() {
         this.observer.disconnect()
     }
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
